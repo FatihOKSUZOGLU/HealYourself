f10499865b8cfadd090677a5f658cc3f13ae1a08
Fatih OKSUZOGLU <fatihoksuzoglu@github>
2023-03-16 23:05:39 +0300

    Initial Commit

---
+++ HealYourself/HealYourselfApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealYourselfApp.swift
+//  HealYourself
+//
+//  Created by Fatih OKSUZOGLU on 16.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct HealYourselfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
