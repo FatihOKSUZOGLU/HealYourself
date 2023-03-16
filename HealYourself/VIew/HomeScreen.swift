58a9d844fb7d6c86e7822b9794966e676577cdfb
Fatih OKSUZOGLU <fatihoksuzoglu@github>
2023-03-16 23:29:08 +0300

    first Commit

--- HealYourself/VIew/HomeScreen.swift
+++ HealYourself/VIew/HomeScreen.swift
@@ -7,13 +7,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Text("HELLOasdasdasd")
         }
         .padding()
     }
@@ -21,6 +22,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HomeView()
     }
 }
